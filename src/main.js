b41914a95b91d1ff2f149b67755ea91bd6bae9bb
邢雅楠 <xingyanan@xingyanandeMacBook-Pro.local>
2020-01-06 20:53:27 +0800

    0106提交

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Component from './components'
+import './utils/register-vant'
+import 'normalize.css'
 
 Vue.use(Component)
 Vue.config.productionTip = false
